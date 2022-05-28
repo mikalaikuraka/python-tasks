63fc2ee21f0f20c30cb331015f60bb0167fc4c67
Mikalai Kuraka <kurakokolia@gmail.com>
2022-05-29 02:12:17 +0300

    add task2

---
+++ second_task.py
@@ -0,0 +1,14 @@
+# Требуется: найти сумму цифр введенного числа при условии, 
+# что оно одно/дву/трех-значное, в противном случае 
+# вывести информацию об ошибке данных.
+
+def sum_of_digits():
+    sum = 0
+    number = int(input("Введите число:"))
+    while number > 0:
+        sum = sum + number % 10
+        number = number // 10
+    return sum
+
+
+print(sum_of_digits())
