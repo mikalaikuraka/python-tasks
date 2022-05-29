01422a6f46350f0dba8ff3cf7a3385c8f3c773b0
Mikalai Kuraka <kurakokolia@gmail.com>
2022-05-29 23:43:28 +0300

    remove magic numbers

--- third_task.py
+++ third_task.py
@@ -3,11 +3,13 @@
 # Если стороны определяют треугольник, найти его площадь. 
 # Если нет, вывести сообщение о неверных данных.
 
+SQRT = 0.5
+
 def get_triangle_square(a, b ,c):
     if b + c > a and a + c > b and a + b > c:
         perimetr = (a + b + c) / 2
 
-        return (perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c)) ** 0.5
+        return (perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c)) ** SQRT
     else:
         return "Неверные данные"
 
