0b66cfaf5c6532cbbd0c408b66540eccfe2d3800
Mikalai Kuraka <kurakokolia@gmail.com>
2022-05-29 23:40:52 +0300

    add task 3

---
+++ third_task.py
@@ -0,0 +1,15 @@
+#Даны: три стороны треугольника.
+# Требуется: проверить, действительно ли это стороны треугольника. 
+# Если стороны определяют треугольник, найти его площадь. 
+# Если нет, вывести сообщение о неверных данных.
+
+def get_triangle_square(a, b ,c):
+    if b + c > a and a + c > b and a + b > c:
+        perimetr = (a + b + c) / 2
+
+        return (perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c)) ** 0.5
+    else:
+        return "Неверные данные"
+
+
+print(get_triangle_square(3,4,5))
